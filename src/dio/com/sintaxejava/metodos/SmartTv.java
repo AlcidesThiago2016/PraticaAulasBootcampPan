faa8bfc46147a54de94fe212a065a02cfa39ab7f
alcid <alcides_thiago@hotmail.com>
2023-02-28 22:08:04 -0300

    Create Package sintaxeJava and class SmartTv.java

---
+++ src/dio/com/sintaxejava/metodos/SmartTv.java
@@ -0,0 +1,9 @@
+package dio.com.sintaxejava.metodos;
+
+public class SmartTv {
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+}
---
+++ src/dio/com/sintaxejava/metodos/Usuario.java
@@ -0,0 +1,12 @@
+package dio.com.sintaxejava.metodos;
+
+public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual ? " + smartTv.canal);
+        System.out.println("Volume Atual ? " + smartTv.volume);
+    }
+}
