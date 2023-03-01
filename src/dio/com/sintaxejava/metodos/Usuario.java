bcddbca2aa40788b11e335252a0322c523310f6f
alcid <alcides_thiago@hotmail.com>
2023-03-01 12:02:10 -0300

    Implements metods for execution class Usuario.java

--- src/dio/com/sintaxejava/metodos/Usuario.java
+++ src/dio/com/sintaxejava/metodos/Usuario.java
@@ -8,5 +8,21 @@ public class Usuario {
         System.out.println("TV Ligada ? " + smartTv.ligada);
         System.out.println("Canal Atual ? " + smartTv.canal);
         System.out.println("Volume Atual ? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> : TV Ligada ? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual ? " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("Volume Atual ? " + smartTv.volume);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> : TV Ligada ? " + smartTv.ligada);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual ? " + smartTv.canal);
+
     }
 }
